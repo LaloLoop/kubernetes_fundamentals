2d41a285d3544fada965b32ca7edf354601181ef
Eduardo Gutiérrez <gt.silva.e@gmail.com>
2020-08-22 22:33:19 -0500

    Add Google provider with project and location

--- main.tf
+++ main.tf
@@ -8,6 +8,14 @@ terraform {
   }
 }
 
+// Provider
+
+provider "google" {
+  project     = "lfs258-287104"
+  region      = "us-central1"
+  version = "~> 3.35.0"
+}
+
 // VPC network
 
 resource "google_compute_network" "vpc_lfclass" {
